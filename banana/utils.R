ca25002f96878d4fa400c8de3885123cd6d40e31
edwaltz <luke.zhang.lz@gmail.com>
2013-01-31 17:22:23 +1100

    Modify banana-shaped random number generator to improve its efficiency for high dimensional data.

--- banana/utils.R
+++ banana/utils.R
@@ -24,11 +24,14 @@ clabc.step <- function(num, p, obs, h, rlik, type="paire", ...) {
   obs.val <- as.vector(obs)
   d <- length(obs.val)  # dimension for summary statistic
   n <- num*h  # number of parameter points
+  
   ret <- list()
   ret$prior <- matrix(runif(num*p, pmin, pmax), nrow=num)
   
+  
   if (type=="full") {
-    sim <- rlik(ndim=d, ...)
+    sim <- matrix(0, nrow=num, ncol=d)  # simulated data
+    rlik(...)
     ret$par <- abc(obs.val, ret$prior, sim, h, "rejection")$unadj.values
     ret$prior <- ret$par[sample(n, num, TRUE), ]+
       mvrnorm(num, rep(0, p), var(ret$par)*(4/((p+2)*n))^(2/(p+4)))
@@ -36,7 +39,7 @@ clabc.step <- function(num, p, obs, h, rlik, type="paire", ...) {
   } else if (type=="pair") {
     order <- combn(d, 2)  # order of the composite likelihood
     for (ind in 1:(d*(d-1)/2)) {
-      sim <- rlik(ndim=d, ...)
+      rlik(...)
       ret$par <- abc(obs.val[order[, ind]], ret$prior, sim[, order[, ind]], h, 
                      "rejection")$unadj.values
       ret$prior <- ret$par[sample(n, num, TRUE), ]+
@@ -52,11 +55,10 @@ clabc.step <- function(num, p, obs, h, rlik, type="paire", ...) {
   return(ret)
 }
 
-rbanana <- function(ndim, b, scale=1) {
+rbanana <- function(b, scale=1) {
   # Simulate from the standard banana distribution.
   #
   # Args:
-  #   ndim - number of dimensions for summary statistics;
   #   b - bananacity;
   #   scale - the scale of the marginal distribution.
   #
@@ -64,27 +66,25 @@ rbanana <- function(ndim, b, scale=1) {
   #   Simulation for the banana likelihood.
   
   p <- ncol(ret$prior)
+  ndim <- ncol(sim)  # number of dimensions for summary statistics
   if (p>ndim) {
     stop("The dim of sumstat must be higher than that of par.")
   }
   n <- nrow(ret$prior)
-  data <- matrix(0, nrow=n, ncol=ndim)  # simulated data
-  data[, 1] <- ret$prior[, 1]+rnorm(n, sd=10)
-  data[, 2] <- ret$prior[, 2]-b*(data[, 1]-ret$prior[, 1])^2+100*b+rnorm(n)
+  sim[, 1] <- ret$prior[, 1]+rnorm(n, sd=10)
+  sim[, 2] <- ret$prior[, 2]-b*(sim[, 1]-ret$prior[, 1])^2+100*b+rnorm(n)
   if (ndim>2) {
-    data[, -c(1, 2)] <- mvrnorm(n, rep(0, ndim-2), diag(ndim-2))
+    sim[, -c(1, 2)] <- mvrnorm(n, rep(0, ndim-2), diag(ndim-2))
   }
   if (p>2) {
-    data[, 3:p]  <- ret$prior[, 3:p] + data[, 3:p]
+    sim[, 3:p]  <- ret$prior[, 3:p] + sim[, 3:p]
   }
-  return(data)
 }
 
-rbanana.rot <- function(ndim, b, scale=1) {
+rbanana.rot <- function(b, scale=1) {
   # Simulate from the rotated banana distribution.
   #
   # Args:
-  #   ndim - number of dimensions for summary statistics;
   #   b - bananacity;
   #   scale - the scale of the marginal distribution.
   #
@@ -92,22 +92,22 @@ rbanana.rot <- function(ndim, b, scale=1) {
   #   Simulation for the banana likelihood.
   
   p <- ncol(ret$prior)
+  ndim <- ncol(sim)  # number of dimensions for summary statistics
   if (p>ndim) {
     stop("The dim of summstat must be higher than that of par.")
   }
   n <- nrow(ret$prior)
-  data <- matrix(0, nrow=n, ncol=ndim)  # simulated data
+  sim <- matrix(0, nrow=n, ncol=ndim)  # simulated data
   rotate.mat <- matrix(c(cos(pi/4), -sin(pi/4), cos(pi/4), sin(pi/4)), nrow=2)
-  data[, 1] <- rnorm(n, sd=10)
-  data[, 2] <- -b*data[, 1]^2+100*b+rnorm(n)
-  data[, 1:2] <- data[, 1:2]%*%rotate.mat+ret$prior[, 1:2]
+  sim[, 1] <- rnorm(n, sd=10)
+  sim[, 2] <- -b*sim[, 1]^2+100*b+rnorm(n)
+  sim[, 1:2] <- sim[, 1:2]%*%rotate.mat+ret$prior[, 1:2]
   if (ndim>2) {
-    data[, -c(1, 2)] <- mvrnorm(n, rep(0, ndim-2), diag(ndim-2))
+    sim[, -c(1, 2)] <- mvrnorm(n, rep(0, ndim-2), diag(ndim-2))
   }
   if (p>2) {
-    data[, 3:p]  <- ret$prior[, 3:p] + data[, 3:p]
+    sim[, 3:p]  <- ret$prior[, 3:p] + sim[, 3:p]
   }
-  return(data)
 }
 
 adj.margin <- function(join, margin) {
