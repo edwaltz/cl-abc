98f6aa5f611f0b7259958fb0c5ac7b68ad1da0e3
edwaltz <luke.zhang.lz@gmail.com>
2013-04-04 16:35:25 +1100

    Add code for 2000 particles.

---
+++ katana/banana/xin-clabc-banana-num-12dim.R
@@ -0,0 +1,19 @@
+# Time test for correlation analysis.
+source("xin-clabc-banana-library.R")
+library(parallel)
+
+num <- 20000000
+p <- 12
+b <- c(0, 0, 0, .01, .01, .01, .05, .05, .05)  # bananacity
+
+ptm.final <- proc.time()  # time record
+
+jobs <- lapply(b, function(x) mcparallel(run.corr(30, x, p, num)))
+ret <- mccollect(jobs)
+save(ret, file="xin-clabc-banana-num-12dim-raw.rda")
+rm(ret)
+
+gc()
+
+cost.final <- proc.time() - ptm.final
+print(cost.final["elapsed"])
