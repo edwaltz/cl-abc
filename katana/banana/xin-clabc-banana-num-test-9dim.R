17dbdfe086f723d5d204afd7f589499f8b23759b
edwaltz <luke.zhang.lz@gmail.com>
2013-04-04 12:47:33 +1100

    Add test code for 2000 particles.

---
+++ katana/banana/xin-clabc-banana-num-test-9dim.R
@@ -0,0 +1,19 @@
+# Time test for correlation analysis.
+source("xin-clabc-banana-library.R")
+library(parallel)
+
+num <- 20000000
+p <- 9
+b <- c(0, .01, .05)  # bananacity
+
+ptm.final <- proc.time()  # time record
+
+jobs <- lapply(b, function(x) mcparallel(get.corr(x, p, num)))
+ret <- mccollect(jobs)
+save(ret, file="xin-clabc-banana-corr-test-9dim.rda")
+rm(ret)
+
+gc()
+
+cost.final <- proc.time() - ptm.final
+print(cost.final["elapsed"])
