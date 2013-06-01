c2abc386d12ef52250e68f0b76d4b566ad9e5fb1
edwaltz_z <luke.zhang.lz@gmail.com>
2013-06-01 22:26:42 +1000

    Improve the corr and num analysis plot.

--- thesis/corr.R
+++ thesis/corr.R
@@ -1,6 +1,4 @@
 # correlation analysis
-# True correlation
-
 
 # 1,000 particles for b = 0, 0.01, 0.05.
 # load data and data clean
@@ -12,26 +10,32 @@ unadj.sd <- apply(unadj.corr, c(1, 2), sd)
 adj.mu <- apply(adj.corr, c(1, 2), mean)
 adj.sd <- apply(adj.corr, c(1, 2), sd)
 
+rho1 <- 0.9780
+rho2 <- 0.9442
+rho3 <- 0.3492
+
 # plot
 # unadjustment
 old <- par(mfrow=c(2, 2))
-matplot(seq(3, 12, by=3), t(unadj.mu), type="l", lty=1:3, col=1, 
-        main="Mean before adjustment", xlab="dimension", ylab="mean")
+
+matplot(seq(3, 12, by=3), t(unadj.mu), type="l", lty=1:3, col=1, ylim=c(0,1),
+        main="Before adjustment", xlab="p", ylab=expression(bar(rho)[m]))
 matpoints(seq(3, 12, by=3), t(unadj.mu), lty=1, col=1, pch=1)
-legend("topright", legend=c("b=0", "b=0.01", "b=0.05"), lty=1:3, cex=0.4)
+abline(h=c(rho1, rho2, rho3), lty=1:3)
+
 matplot(seq(3, 12, by=3), t(unadj.sd), type="l", lty=1:3, col=1, 
-        main="Standard deviation before adjustment", xlab="dimension", ylab="standard deviation")
+        main="Before adjustment", xlab="p", ylab=expression(hat(sd)(rho[m])))
 matpoints(seq(3, 12, by=3), t(unadj.sd), lty=1, col=1, pch=1)
-legend("topleft", legend=c("b=0", "b=0.01", "b=0.05"), lty=1:3, cex=0.4)
+
 # adjustment
-matplot(seq(3, 12, by=3), t(adj.mu), type="l", lty=1:3, col=1, 
-        main="Mean after adjustment", xlab="dimension", ylab="mean")
+matplot(seq(3, 12, by=3), t(adj.mu), type="l", lty=1:3, col=1, ylim=c(0,1),
+        main="After adjustment", xlab="p", ylab=expression(bar(rho)[m]^a))
 matpoints(seq(3, 12, by=3), t(adj.mu), lty=1, col=1, pch=1)
-legend("topright", legend=c("b=0", "b=0.01", "b=0.05"), lty=1:3, cex=0.4)
+abline(h=c(rho1, rho2, rho3), lty=1:3)
 matplot(seq(3, 12, by=3), t(adj.sd), type="l", lty=1:3, col=1, 
-        main="Standard deviation after adjustment", xlab="dimension", ylab="standard deviation")
+        main="After adjustment", xlab="p", ylab=expression(hat(sd)(rho[m]^a)))
 matpoints(seq(3, 12, by=3), t(adj.sd), lty=1, col=1, pch=1)
-legend("topleft", legend=c("b=0", "b=0.01", "b=0.05"), lty=1:3, cex=0.4)
+
 par(old)
 
 # 1,000, 5,000 and 10,000 particles for b =0.01.
@@ -47,21 +51,21 @@ adj.sd <- apply(adj.num, c(1, 2), sd)
 # plot
 # unadjustment
 old <- par(mfrow=c(2, 2))
-matplot(seq(3, 12, by=3), t(unadj.mu), type="l", lty=1:3, col=1, 
-        main="Mean before adjustment", xlab="dimension", ylab="mean")
+
+matplot(seq(3, 12, by=3), t(unadj.mu), type="l", lty=1:3, col=1, ylim=c(0,1), 
+        main="Before adjustment", xlab="p", ylab=expression(bar(rho)[m]))
 matpoints(seq(3, 12, by=3), t(unadj.mu), lty=1, col=1, pch=1)
-legend("topright", legend=c("1,000 paricles", "5,000 paricles", "10,000 paricles"), lty=1:3, cex=0.4)
+abline(h=rho2)
 matplot(seq(3, 12, by=3), t(unadj.sd), type="l", lty=1:3, col=1, 
-        main="Standard deviation before adjustment", xlab="dimension", ylab="standard deviation")
+        main="Before adjustment", xlab="p", ylab=expression(hat(sd)(rho[m])))
 matpoints(seq(3, 12, by=3), t(unadj.sd), lty=1, col=1, pch=1)
-legend("topleft", legend=c("1,000 paricles", "5,000 paricles", "10,000 paricles"), lty=1:3, cex=0.4)
 # adjustment
-matplot(seq(3, 12, by=3), t(adj.mu), type="l", lty=1:3, col=1, 
-        main="Mean after adjustment", xlab="dimension", ylab="mean")
+matplot(seq(3, 12, by=3), t(adj.mu), type="l", lty=1:3, col=1, ylim=c(0,1), 
+        main="After adjustment", xlab="p", ylab=expression(bar(rho)[m]^a))
 matpoints(seq(3, 12, by=3), t(adj.mu), lty=1, col=1, pch=1)
-legend("topright", legend=c("1,000 paricles", "5,000 paricles", "10,000 paricles"), lty=1:3, cex=0.4)
+abline(h=rho2)
 matplot(seq(3, 12, by=3), t(adj.sd), type="l", lty=1:3, col=1, 
-        main="Standard deviation after adjustment", xlab="dimension", ylab="standard deviation")
+        main="After adjustment", xlab="p", ylab=expression(hat(sd)(rho[m]^a)))
 matpoints(seq(3, 12, by=3), t(adj.sd), lty=1, col=1, pch=1)
-legend("topleft", legend=c("1,000 paricles", "5,000 paricles", "10,000 paricles"), lty=1:3, cex=0.4)
+
 par(old)
