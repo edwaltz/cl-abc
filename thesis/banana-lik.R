37fac7c9abbe50cd81b33c80d4114a161d46b50d
edwaltz_z <luke.zhang.lz@gmail.com>
2013-06-01 21:44:02 +1000

    Add contour plots for 2 types of banana-shaped distribution.

--- thesis/banana-lik.R
+++ thesis/banana-lik.R
@@ -17,22 +17,51 @@ pmin <- -25
 pmax <- 25
 lim.x <- c(pmin, pmax)
 lim.y <- c(pmin, pmax)
-b <- .01  # bananacity
 
 grid.x <- seq(lim.x[1], lim.x[2], .5)
 grid.y <- seq(lim.y[1], lim.y[2], .5)
 grid.z1 <- matrix(0, nrow=length(grid.x), ncol=length(grid.y))
 grid.z2 <- matrix(0, nrow=length(grid.x), ncol=length(grid.y))
 
+
+
+old <- par(mfrow=c(3, 2))
+
+# standard B=0
 for (ind in 1:length(grid.x)) {
-  grid.z1[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.s, b=b)
-  grid.z2[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.r, b=b)
+  grid.z1[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.s, 0)
 }
-
-old <- par(mfrow=c(1, 2))
-
-contour(grid.x, grid.y, grid.z1, main="Standard likelihood", xlab=expression(theta[(1)]), 
+contour(grid.x, grid.y, grid.z1, main="Standard likelihood, B=0", xlab=expression(theta[(1)]), 
         ylab=expression(theta[(2)]), xlim=lim.x, ylim=lim.y, asp=1)
-contour(grid.x, grid.y, grid.z2, main="Modified likelihood", xlab=expression(theta[(1)]), 
+# rotated B=0
+for (ind in 1:length(grid.x)) {
+  grid.z2[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.r, 0)
+}
+contour(grid.x, grid.y, grid.z2, main="Modified likelihood, B=0", xlab=expression(theta[(1)]), 
         ylab=expression(theta[(2)]), xlim=lim.x, ylim=lim.y, asp=1)
+# standard B=0.01
+for (ind in 1:length(grid.x)) {
+  grid.z1[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.s, 0.01)
+}
+contour(grid.x, grid.y, grid.z1, main="Standard likelihood, B=0.01", xlab=expression(theta[(1)]), 
+        ylab=expression(theta[(2)]), xlim=lim.x, ylim=lim.y, asp=1)
+# rotated B=0.01
+for (ind in 1:length(grid.x)) {
+  grid.z2[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.r, 0.01)
+}
+contour(grid.x, grid.y, grid.z2, main="Modified likelihood, B=0.01", xlab=expression(theta[(1)]), 
+        ylab=expression(theta[(2)]), xlim=lim.x, ylim=lim.y, asp=1)
+# standard B=0.05
+for (ind in 1:length(grid.x)) {
+  grid.z1[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.s, 0.05)
+}
+contour(grid.x, grid.y, grid.z1, main="Standard likelihood, B=0.05", xlab=expression(theta[(1)]), 
+        ylab=expression(theta[(2)]), xlim=lim.x, ylim=lim.y, asp=1)
+# rotated B=0.05
+for (ind in 1:length(grid.x)) {
+  grid.z2[ind, ] <- apply(cbind(grid.x[ind], grid.y),1, dbanana.r, 0.05)
+}
+contour(grid.x, grid.y, grid.z2, main="Modified likelihood, B=0.05", xlab=expression(theta[(1)]), 
+        ylab=expression(theta[(2)]), xlim=lim.x, ylim=lim.y, asp=1)
+
 par(old)
